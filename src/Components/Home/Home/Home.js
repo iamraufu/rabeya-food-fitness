48bd687881aef677e74dd352a1f3974eb0d96107
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-27 23:56:23 +0600

    Navbar added

---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Navbar from '../../Shared/Navbar/Navbar';
+import Footer from '../../Shared/Footer/Footer';
+
+const Home = () => {
+      return (
+            <div>
+                  <Navbar />
+                  <Header/>
+                  <Footer/>
+            </div>
+      );
+};
+
+export default Home;
