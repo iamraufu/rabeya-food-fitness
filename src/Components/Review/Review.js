560e86ac4df88c5e5b9f74da3fe7aa50d946e227
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-05-01 23:40:27 +0600

    review added

---
+++ src/Components/Review/Review.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Review = () => {
+      return (
+            <div>
+                  <h1>Give your valuable feedback to us</h1>
+            </div>
+      );
+};
+
+export default Review;
