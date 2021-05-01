8e89208cb7a819ed66fd6f91ac9be6ae210b5917
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-05-01 23:40:48 +0600

    consultation added

---
+++ src/Components/Consultation/Consultation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar/Navbar';
+
+const Consultation = () => {
+      return (
+            <div className='bg-dark' style={{height:"100vh"}}>
+                  <Navbar/>
+                  <h1 className='text-center text-info pt-5'>One to one Consultation will be live here</h1>
+            </div>
+      );
+};
+
+export default Consultation;
