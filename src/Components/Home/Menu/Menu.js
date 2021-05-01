e27eb45f590fb911e3401d78946cf9478ece2f01
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-05-01 23:43:45 +0600

    menu updated

--- src/Components/Home/Menu/Menu.js
+++ src/Components/Home/Menu/Menu.js
@@ -1,59 +1,39 @@
 import './Menu.css';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 
-const menuData = [
-      {
-            name:'Chicken with Vegetables',
-            price:12.99,
-            img:"http://fitmeal.like-themes.com/wp-content/uploads/2018/02/dish_07-1-360x360.jpg"
-      },
-      {
-            name:'Grilled Chicken Breast',
-            price:9.49,
-            img:"http://fitmeal.like-themes.com/wp-content/uploads/2018/02/dish_08-1-360x360.jpg"
-      },
-      {
-            name:'Salad with Mozarella',
-            price:8.99,
-            img:"http://fitmeal.like-themes.com/wp-content/uploads/2018/02/dish_03-1-360x360.jpg"
-      },
-      {
-            name:'Red beetroot soup',
-            price:7.49,
-            img:"http://fitmeal.like-themes.com/wp-content/uploads/2018/02/dish_04-1-360x360.jpg"
-      },
-      {
-            name:'Steaks with Asparagus',
-            price:10,
-            img:"http://fitmeal.like-themes.com/wp-content/uploads/2018/02/dish_05-1-360x360.jpg"
-      },
-      {
-            name:'Tender rabbit soup',
-            price:6.49,
-            img:"http://fitmeal.like-themes.com/wp-content/uploads/2018/02/dish_06-1-360x360.jpg"
-      }
-]
 const Menu = () => {
+      const history = useHistory();
+      const [food, setFood] = useState([]);
+
+      useEffect(() => {
+            fetch('http://localhost:5000/foods')
+                  .then(res => res.json())
+                  .then(data => setFood(data))
+      }, [])
+
+      const handleClick = id => {
+            history.push(`/foods/${id}`)
+      }
+
       return (
             <section className='container mt-5'>
-                  <h1 style={{color:'#2cb978'}} className='fw-bold text-center'>Popular Dishes</h1>
+                  <h1 style={{ color: '#2cb978' }} className='fw-bold text-center'>Popular Dishes</h1>
                   <div className="d-flex flex-wrap">
-                  {
-                        menuData.map(md=>
-                              <div className="menu col-md-4 mt-5">
-                                    <img src={md.img} alt=""/>
-                                    <h3 className='fw-bold text-center'>{md.name}</h3>
-                                    <p className='fw-bold text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur, nihil.</p>
-                                    <h5 className='fw-bold text-center text-danger'>Price: ${md.price}</h5>
-                                    <div className="position-relative mt-5">
-                                          <div className="position-absolute top-50 start-50 translate-middle">
-                                    <Link className='mt-5' to='/appointment'><button className='btn btn-read btn-brand text-white fw-bold'>Add to Cart</button></Link>
+                        {
+                              food.map(md =>
+                                    <div className="menu col-md-4 mt-5">
+                                          <img src={md.imageURL} alt="" />
+                                          <h3 className='fw-bold text-center'>{md.name}</h3>
+                                          <h5 className='fw-bold text-center text-danger'>Price: ${md.price}</h5>
+                                          <div className="position-relative mt-5">
+                                                <div className="position-absolute top-50 start-50 translate-middle">
+                                                      <button onClick={()=>handleClick(md._id)} className='btn btn-read btn-brand text-white fw-bold mt-5'>Add to Cart</button>
+                                                </div>
                                           </div>
                                     </div>
-                              </div>
                               )
-                  }
+                        }
                   </div>
             </section>
       );
